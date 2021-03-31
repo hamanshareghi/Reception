399829bcf59154cc863fb9ec0bf9470265598520
hamanshareghi <haman.shareghi@gmail.com>
2021-03-31 19:45:08 +0430

    Save Changes In Customer

--- Reception/DataAccess/Interfaces/ICustomer.cs
+++ Reception/DataAccess/Interfaces/ICustomer.cs
@@ -10,6 +10,6 @@ namespace DataAccess.Interfaces
     public interface ICustomer
     {
         Tuple<List<ApplicationUser>,int> GetAll(int pageId=1);
-        List<ApplicationUser> GetUserBySearch(string search,int pageId=1);
+        Tuple<List<ApplicationUser>,int> GetUserBySearch(string search,int pageId=1);
     }
 }
--- Reception/DataAccess/Services/CustomerService.cs
+++ Reception/DataAccess/Services/CustomerService.cs
@@ -43,18 +43,40 @@ namespace DataAccess.Services
             return Tuple.Create(query,pageCount);
         }
 
-        public List<ApplicationUser> GetUserBySearch(string search,int pageId=1)
+        public Tuple<List<ApplicationUser>,int> GetUserBySearch(string search,int pageId=1)
         {
-            IQueryable<ApplicationUser> model = _userManager.Users
-                .OrderByDescending(s=>s.UpDateTime);
-            model = model.Where(
+            int take = 0;
+            if (take == 0)
+                take = 30;
+            int skip = (pageId - 1) * take;
+            int pageCount = _userManager.Users
+                .Count(s => s.FullName.ToLower().Contains(search)
+                            || s.Contact.ToLower().Contains(search)
+                            || s.Address.ToLower().Contains(search)
+                            || s.Email.ToLower().Contains(search)
+                            || s.PhoneNumber.ToLower().Contains(search));
+            if (pageCount % take != 0)
+            {
+                pageCount = pageCount / take;
+                pageCount++;
+            }
+            else
+            {
+                pageCount = pageCount / take;
+            }
+            var  model = _userManager.Users
+                .OrderByDescending(s=>s.UpDateTime)
+                .Where(
                 s=>s.FullName.ToLower().Contains(search)
                 || s.Contact.ToLower().Contains(search)
                 || s.Address.ToLower().Contains(search)
                 || s.Email.ToLower().Contains(search)
                 || s.PhoneNumber.ToLower().Contains(search)
-            );
-            return model.ToList();
+            )
+                .Skip(skip)
+                .Take(take)
+                .ToList();
+            return Tuple.Create(model, pageCount);
         }
     }
 }
